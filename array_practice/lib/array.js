be4aaccc39bfc99da463625a72c661c7b0130b88
Mishal Islam <mishal.islam01@gmail.com>
2015-01-14 18:36:18 +0000

    iterates through an array with duplicates and displays element and index

--- array_practice/lib/array.js
+++ array_practice/lib/array.js
@@ -9,9 +9,61 @@ TestArray.prototype.show = function() {
 };
 
 TestArray.prototype.iterateThroughArray = function() {
-  var text =""
+  var text = ""
   for(num = 0; num < this.array.length; num++){
     text+= this.array[num] + " "
   }
+  return text
+};
+
+TestArray.prototype.iterateUsingForEach = function() {
+  var text = ""
+  this.array.forEach(function(element){
+    text += element + " "
+  });
+  return text
+};
+
+TestArray.prototype.printEvenElements = function() {
+  var text = ""
+  for(num = 0; num < this.array.length; num+=2){
+    text += this.array[num] + " "
+  }
+  return text
+};
+
+TestArray.prototype.forEachEvenElements = function() {
+  var text = ""
+  var array = this.array
+
+  this.array.forEach(function(element){
+    var index = array.indexOf(element)
+    if(index % 2 === 0){
+      text += element + " "
+    }
+    array[index] = 1
+  });
+  return text
+};
+
+TestArray.prototype.printIndex = function() {
+  var text = ""
+  var array = this.array
+  for(num = 0; num < array.length; num++){
+    var index = array.indexOf(array[num])
+    text += index + ":" + array[num] + " "
+    array[index] = 1
+  }
+  return text
+};
+
+TestArray.prototype.forEachPrintIndex = function() {
+  var text = ""
+  var array = this.array
+  array.forEach(function(element){
+    var index = array.indexOf(element)
+    text += index + ":" + element + " "
+    array[index] = 1
+  });
   return text
 };
--- array_practice/spec/arraySpec.js
+++ array_practice/spec/arraySpec.js
@@ -6,13 +6,32 @@ describe("Array Practice", function(){
     array.setArray(['a', 'b', 'c', 'd', 'h', 'f', 'l', 'j', 'p', 'o', 'i', 'k', 'm', 'i', 'l', 'o', 'g', 's', 'x' ,'k'])
   });
 
-
    it("should set an array", function(){
     expect(array.show()).toEqual(['a', 'b', 'c', 'd', 'h', 'f', 'l', 'j', 'p', 'o', 'i', 'k', 'm', 'i', 'l', 'o', 'g', 's', 'x' ,'k'])
   });
 
   it("should iterate through an array and print each element", function(){
     expect(array.iterateThroughArray()).toEqual("a b c d h f l j p o i k m i l o g s x k ")
+  });
+
+  it("should iterate through an array using forEach", function(){
+    expect(array.iterateUsingForEach()).toEqual("a b c d h f l j p o i k m i l o g s x k ")
+  });
+
+  it("should iterate through an array and print only the even elements", function(){
+    expect(array.printEvenElements()).toEqual("a c h l p i m l g x ")
+  });
+
+  it("should iterate through an array and print only the even elements using forEach", function(){
+    expect(array.forEachEvenElements()).toEqual("a c h l p i m l g x ")
+  });
+
+  it("should iterate through an array and prints the element and the index", function(){
+    expect(array.printIndex()).toEqual("0:a 1:b 2:c 3:d 4:h 5:f 6:l 7:j 8:p 9:o 10:i 11:k 12:m 13:i 14:l 15:o 16:g 17:s 18:x 19:k ")
+  });
+
+  it("should iterate through an array and prints the element and the index using forEach", function(){
+    expect(array.forEachPrintIndex()).toEqual("0:a 1:b 2:c 3:d 4:h 5:f 6:l 7:j 8:p 9:o 10:i 11:k 12:m 13:i 14:l 15:o 16:g 17:s 18:x 19:k ")
   });
 
 });
